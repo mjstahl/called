de820767c9ad96c76978c06aa3782474a1dc4593
Mark Stahl <mjstahl@gmail.com>
2018-09-05 15:30:32 -0400

    Convert to standardjs

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-function called(compute) {
-  const fn = function() {
-    let error = null, result = null, self = this;
+function called (compute) {
+  const fn = function () {
+    let error = null; let result = null; let self = this
     try {
-      result = compute.apply(this, arguments);
+      result = compute.apply(this, arguments)
     } catch (e) {
-      error = e;
+      error = e
     }
-    setTimeout(() => handlers.forEach((h) => h.call(self, error, result)), 0);
-    return result;
-  };
+    setTimeout(() => handlers.forEach((h) => h.call(self, error, result)), 0)
+    return result
+  }
 
-  const handlers = [];
-  fn.subscribe = function(callback) {
-    handlers.push(callback);
-    return () => void handlers.splice(handlers.indexOf(callback) >>> 0, 1);
-  };
-  return fn;
+  const handlers = []
+  fn.subscribe = function (callback) {
+    handlers.push(callback)
+    return () => void handlers.splice(handlers.indexOf(callback) >>> 0, 1)
+  }
+  return fn
 };
 
-module.exports = called;
+module.exports = called
